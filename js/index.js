316b7d19a91072fcae2394f43e36304dc4093c53
Laven Morrow <lmorrow13@live.com>
2018-01-31 13:16:43 -0700

    finished build

--- js/index.js
+++ js/index.js
@@ -1,34 +1,50 @@
 $(function() {
-	$('#value1').on('click', function() {
+	$('#ship1').on('click', function() {
 		if (Cookies.get('cook') == undefined) {
 			Cookies.set('cook', 0)
 		}
 		Cookies.set('cook', parseInt(Cookies.get('cook')) + 1);
-		$('#value1 h1').text(Cookies.get('cook'));
+		$('#ship1 h1').text(Cookies.get('cook'));
+		$('#boom1').animate({opacity: '1'});
+		$('#boom1').animate({opacity: '0'}, 1300);
+		$('#ship1death').animate({opacity: '1'});
+		$('#ship1death').animate({opacity: '0'}, 300);
 	})
 
-	$('#value2').on('click', function() {
+	$('#ship2').on('click', function() {
 		if (Cookies.get('make') == undefined) {
 			Cookies.set('make', 0)
 		}
 		Cookies.set('make', parseInt(Cookies.get('make')) + 1);
-		$('#value2 h1').text(Cookies.get('make'));
+		$('#ship2 h1').text(Cookies.get('make'));
+		$('#boom2').animate({opacity: '1'});
+		$('#boom2').animate({opacity: '0'}, 1300);
+		$('#ship2death').animate({opacity: '1'});
+		$('#ship2death').animate({opacity: '0'}, 300);
 	})
 
-	$('#value3').on('click', function() {
+	$('#ship3').on('click', function() {
 		if (Cookies.get('this') == undefined) {
 			Cookies.set('this', 0)
 		}
 		Cookies.set('this', parseInt(Cookies.get('this')) + 1);
-		$('#value3 h1').text(Cookies.get('this'));
+		$('#ship3 h1').text(Cookies.get('this'));
+		$('#boom3').animate({opacity: '1'});
+		$('#boom3').animate({opacity: '0'}, 1300);
+		$('#ship3death').animate({opacity: '1'});
+		$('#ship3death').animate({opacity: '0'}, 300);
 	})
 
 	$('#clearClick').on('click', function() {
 		Cookies.set('cook', 0);
 		Cookies.set('make', 0);
 		Cookies.set('this', 0);
-		$('#value1 h1').text(0);
-		$('#value2 h1').text(0);
-		$('#value3 h1').text(0);
+		$('#ship1 h1').text(0);
+		$('#ship2 h1').text(0);
+		$('#ship3 h1').text(0);
+		$('.laser-beam').animate({opacity: '1'});
+		$('.laser-beam').animate({opacity: '0'}, 300);
+		$('#monsterBoom').animate({opacity: '1'});
+		$('#monsterBoom').animate({opacity: '0'}, 1300);
 	})
 })
